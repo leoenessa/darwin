7e41c729c81292d6546cb839662517454a485370
Leonardo <leonardooc@globo.com>
2017-06-14 16:44:55 -0300

    Funcionando agora a animação
    Depois das alterações, primordialmente o mundo[:] = new_mundo[:], a animação funciona como deveria. Falta agora criar as regras do mundo

--- darwin.py
+++ darwin.py
@@ -7,23 +7,40 @@ ON = 255
 OFF = 0
 
 def criaMundo(N):
-	#return(np.random.choice([ON,OFF],N*N,p=[0.5,0.5]).reshape(N,N))
-	return(np.random.choice([ON,OFF],N*N,p=[0.5,0.5]).reshape(4,4))
+	return(np.random.choice([ON,OFF],N*N,p=[0.5,0.5]).reshape(N,N))
+	
 
-def atualizaMundo(mundo,N):
-    mundo = np.random.choice([ON,OFF],N*N,p=[0.3,0.7].reshape(4,4))
-    return mundo
+def atualizaMundo(frameNum,N,mundo,img):
+	total = 0
+	new_mundo = mundo.copy()
+	for linha in range(new_mundo.shape[0]):
+		for col in range(new_mundo.shape[1]):
+			if(new_mundo[linha,col]==0):
+				total+=1
+	new_mundo = np.random.choice([ON,OFF],N*N,p=[0.5,0.5]).reshape(N,N)
+	img.set_data(mundo)
+	#mundo[:]=new_mundo[:]
+	#mundo=new_mundo
+	mundo = new_mundo.copy()
+	print(mundo)
+	print(new_mundo)
+	print(total)
+	return(img,)
 
 def main():
-
-	N = 4
-	mundo = criaMundo(N)
-	fig1,ax = plt.subplots()
-	img = ax.imshow(mundo)
-		
-	animacao = animation.FuncAnimation(fig1, atualizaMundo, (mundo,N,))
-	
-	plt.show()
+	try:		
+		N = 4
+		mundo = criaMundo(N)
+		print(mundo)
+		fig1,ax = plt.subplots()
+		img = ax.imshow(mundo)
+		animacao = animation.FuncAnimation(fig1, atualizaMundo, fargs=(N,mundo,img,), blit=True)
+		plt.show()
+	except Exception as ex:
+		print("DEU MERDA AQUI FORA!")
 
 if __name__ == '__main__':
-	main()
+	try:
+		main()
+	except Exception as fuyck:
+		pass
