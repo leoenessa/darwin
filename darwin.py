5b51303d2ef741eb224906184bd3283a114e998d
Leonardo <leonardooc@globo.com>
2017-06-22 10:06:05 -0300

    Adição de uma pseudo regra de conflito

--- darwin.py
+++ darwin.py
@@ -3,41 +3,50 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import time
 
-ON = 255
-OFF = 0
+ON = 255 #forte
+OFF = 0  #fraco
 
 def criaMundo(N):
 	return(np.random.choice([ON,OFF],N*N,p=[0.5,0.5]).reshape(N,N))
 	
 
-def atualizaMundo(frameNum,N,mundo,img):
-	total = 0
+def atualizaMundo(frameNum, mundo,N,img):
 	new_mundo = mundo.copy()
-	for linha in range(new_mundo.shape[0]):
-		for col in range(new_mundo.shape[1]):
-			if(new_mundo[linha,col]==0):
-				total+=1
-	new_mundo = np.random.choice([ON,OFF],N*N,p=[0.5,0.5]).reshape(N,N)
+	total = 0
+	for l in range(new_mundo.shape[0]):
+		for c in range(new_mundo.shape[1]):
+			total= (new_mundo[(l-1)%N,(c-1)%N]+new_mundo[(l-1)%N,c]+new_mundo[(l-1)%N,(c+1)%N]+
+			new_mundo[l,(c-1)%N]+new_mundo[l,(c+1)%N]+
+			new_mundo[(l+1)%N,(c-1)%N]+new_mundo[(l+1)%N,c]+new_mundo[(l+1)%N,(c+1)%N])	
+			'''
+			Se for forte (ON) mas houver outro forte ao lado, se torna fraco.
+			Se for fraco e houver no maximo um forte, se torna forte
+			'''
+			if(new_mundo[l,c]==ON):
+				if(total>=ON):
+					new_mundo[l,c]=OFF
+			else:
+				if(total<=ON):
+					new_mundo[l,c]=ON
+				else:
+					pass
+					#print("%d: %d"%(total,int(new_mundo[l,c])))
+
+	mundo[:]=new_mundo[:]
 	img.set_data(mundo)
-	#mundo[:]=new_mundo[:]
-	#mundo=new_mundo
-	mundo = new_mundo.copy()
-	print(mundo)
-	print(new_mundo)
-	print(total)
 	return(img,)
 
 def main():
 	try:		
-		N = 4
+		N = 50
 		mundo = criaMundo(N)
 		print(mundo)
-		fig1,ax = plt.subplots()
+		fig1,ax= plt.subplots()
 		img = ax.imshow(mundo)
-		animacao = animation.FuncAnimation(fig1, atualizaMundo, fargs=(N,mundo,img,), blit=True)
+		animacao = animation.FuncAnimation(fig1, atualizaMundo, fargs=(mundo,N,img,), blit=True)
 		plt.show()
 	except Exception as ex:
-		print("DEU MERDA AQUI FORA!")
+		 print(str(ex))
 
 if __name__ == '__main__':
 	try:
